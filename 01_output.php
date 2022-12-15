ea581b20135b5823b62d59ad991ffc4f807d3dab
XILE-CAI <c358143455@gmail.com>
2022-12-15 22:41:16 +1030

    php practice record

---
+++ 01_output.php
@@ -0,0 +1,18 @@
+<?php
+// echo
+ echo 'Hello', 123, 10.6;
+
+ // print --- single argument
+ print 123;
+
+ //print_r() -single value and arrays
+ print_r([1,2,3]);
+
+ //var_dump()- return more info data type and length
+ var_dump('hello');
+ var_dump(true);
+
+ //var_export()
+ var_export('hello');
+?>
+
